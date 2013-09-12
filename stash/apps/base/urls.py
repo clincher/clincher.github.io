161f3aea6a78c2e4b6bdbf58f8b0c1605d384e41
clincher <va.bolshakov@gmail.com>
2013-09-12 20:00:14 +0400

    First pages commit

--- stash/apps/base/urls.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'ubunta'
-from django.conf.urls import patterns, include, url
-from views import HomeView
-
-urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name='home'),
-)
